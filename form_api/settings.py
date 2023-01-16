84b3ce33d71470eccd0ff53efc50eaea6f781ccf
prashantmore277 <prashantmore277@gmail.com>
2023-01-16 14:01:16 +0530

    adding vercel setting

--- form_api/settings.py
+++ form_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!)47k)*fumd4l(8=-b%*_v0z_od#54-$p8a6#n%775-z0_=2=1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
 
 CORS_ORIGIN_ALLOW_ALL=True
 
@@ -125,9 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
